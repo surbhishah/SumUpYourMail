ccabda7ef1e92f466c685e3a37583585526d8c97
surbhishah <surbhishah81@gmail.com>
2015-06-05 15:38:59 +0530

    Basic UI made. WebJobs SDK and Gmail API Nuget

---
+++ SumUpYourMail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SumUpYourMail.Startup))]
+namespace SumUpYourMail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
