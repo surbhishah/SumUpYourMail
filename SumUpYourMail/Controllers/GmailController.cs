9ba304fe1d4d7eedec08834afbae62a05d17bb1c
surbhishah <surbhishah81@gmail.com>
2015-06-09 14:28:38 +0530

    UI finished

---
+++ SumUpYourMail/Controllers/GmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SumUpYourMail.Controllers
+{
+    public class GmailController : Controller
+    {
+        // GET: Gmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
